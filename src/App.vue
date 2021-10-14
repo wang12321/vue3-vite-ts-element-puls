8259ecbc5e2ce7310410ce052562b991163c5c3c
wlg <1396933943@qq.com>
2021-10-14 09:22:58 +0800

    fix: 集成husky

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
